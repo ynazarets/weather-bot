8bb405b1b95ffe54b06dbe4bbc5333d323f65a17
Yehor Nazarets <nazarets1111@gmail.com>
2026-01-05 15:25:13 +0100

    init project

---
+++ src/main/java/weather/weathertelegrambot/WeatherTelegramBotApplication.java
@@ -0,0 +1,13 @@
+package weather.weathertelegrambot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherTelegramBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherTelegramBotApplication.class, args);
+    }
+
+}
